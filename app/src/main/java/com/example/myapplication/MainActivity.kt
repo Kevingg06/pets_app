1c2ebd4465f538b91e28cbaba468628999e017ad
Kevingg06 <kevingonzalezr433@gmail.com>
2024-05-17 18:30:05 -0300

    Creacion de la arquitectura y el consumo de la API con la mainActivity y su recyclerView

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.viewModel.DogViewModel
+import com.example.myapplication.viewModel.StateViewModel
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
-    val viewModel by  viewModels<DogViewModel>()
+    private val viewModel by viewModels<DogViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,11 +28,43 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        initRecyclerView()
+        call()
+        observer()
     }
 
-    private fun initRecyclerView(){
-        val adapter = DogsAdapter()
+    private fun call() {
+        viewModel.getDogs()
+    }
+
+    private fun observer() {
+        viewModel.data.observe(this) { data ->
+            when (data) {
+                is StateViewModel.Success -> {
+                    hideLoading()
+                    initRecyclerView(data.info.message ?: listOf())
+                }
+
+                is StateViewModel.loading -> {
+                    showLoading()
+                }
+
+                is StateViewModel.Error -> {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun showLoading(){
+        binding.loadingScreen.visibility = View.VISIBLE
+    }
+
+    private fun hideLoading(){
+        binding.loadingScreen.visibility = View.GONE
+    }
+
+    private fun initRecyclerView(value: List<String>) {
+        val adapter = DogsAdapter(value)
         binding.recyclerDogs.adapter = adapter
     }
 }
--- app/src/main/java/com/example/myapplication/viewModel/StateViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewModel/StateViewModel.kt
@@ -3,7 +3,7 @@ package com.example.myapplication.viewModel
 import com.example.myapplication.response.DogResponse
 
 sealed class StateViewModel() {
-    data class Success(private val info: DogResponse) : StateViewModel()
-    data class Error(private val message: String) : StateViewModel()
+    data class Success( val info: DogResponse) : StateViewModel()
+    data class Error( val message: String) : StateViewModel()
     data object loading : StateViewModel()
 }
